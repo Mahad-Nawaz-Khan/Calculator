98a957aa8d77b28e8414d7e40b0bfaa482dd934d
Mahad-Nawaz_Khan <mahadnawaz2006@gmail.com>
2024-04-15 15:18:46 +0500

    GPT Calulator has its own folder to run separately

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var inquirer_1 = require("inquirer");
-var chalk_1 = require("chalk");
+import inquirer from "inquirer";
+import chalk from "chalk";
 console.log("\n");
-var q1 = await inquirer_1.default.prompt([{
+let q1 = await inquirer.prompt([{
         message: "First Value",
         type: "number",
         name: "first",
-        prefix: chalk_1.default.green("% "),
+        prefix: chalk.green("% "),
     },
     {
         message: "Second Value",
         type: "number",
         name: "second",
-        prefix: chalk_1.default.green("% "),
+        prefix: chalk.green("% "),
     },
     {
         message: "What operation do you want to use? \n\n",
         type: "list",
         name: "options",
         choices: ["Addition", "Subtraction", "Multiplication", "Division", "Modulo"],
-        prefix: chalk_1.default.green("# "),
+        prefix: chalk.green("# "),
     }]);
+console.log("\n");
+console.log("First value:", chalk.magentaBright(q1.first));
+console.log("Second value:", chalk.magentaBright(q1.second));
+console.log("Operation:", chalk.magentaBright(q1.options));
 if (q1.options === "Addition") {
-    var ans = q1.first + q1.second;
-    console.log(chalk_1.default.yellowBright("\n" + ans.toFixed(2)));
+    let ans = q1.first + q1.second;
+    console.log("\nResult: " + chalk.yellowBright(ans.toFixed(2)));
 }
 else if (q1.options === "Subtraction") {
-    var ans = q1.first - q1.second;
-    console.log(chalk_1.default.yellowBright("\n" + ans.toFixed(2)));
+    let ans = q1.first - q1.second;
+    console.log("\Result: n" + chalk.yellowBright(ans.toFixed(2)));
 }
 else if (q1.options === "Multiplication") {
-    var ans = q1.first * q1.second;
-    console.log(chalk_1.default.yellowBright("\n" + ans.toFixed(2)));
+    let ans = q1.first * q1.second;
+    console.log("\nResult: " + chalk.yellowBright(ans.toFixed(2)));
 }
 else if (q1.options === "Division") {
-    var ans = q1.first / q1.second;
-    console.log(chalk_1.default.yellowBright("\n" + ans.toFixed(2)));
+    let ans = q1.first / q1.second;
+    console.log("\nResult: " + chalk.yellowBright(ans.toFixed(2)));
 }
 else if (q1.options === "Modulo") {
-    var ans = q1.first % q1.second;
-    console.log(chalk_1.default.yellowBright("\n" + ans.toFixed(2)));
+    let ans = q1.first % q1.second;
+    console.log("\nResult: " + chalk.yellowBright(ans.toFixed(2)));
 }
--- index.ts
+++ index.ts
@@ -27,35 +27,41 @@ let q1 = await inquirer.prompt([{
     prefix: chalk.green("# "),
 }]);
 
+console.log("\n");
+console.log("First value:", chalk.magentaBright(q1.first));
+console.log("Second value:",chalk.magentaBright(q1.second));
+console.log("Operation:", chalk.magentaBright(q1.options));
+
+
 if(
     q1.options === "Addition")
     {
         let ans: number= q1.first + q1.second;
-        console.log(chalk.yellowBright("\n" + ans.toFixed(2)));
+        console.log("\nResult: " + chalk.yellowBright(ans.toFixed(2)));
     }
     else if(
         q1.options === "Subtraction")
         {
             let ans: number= q1.first - q1.second;
-            console.log(chalk.yellowBright("\n" + ans.toFixed(2)));
+            console.log("\Result: n" + chalk.yellowBright(ans.toFixed(2)));
         }
         else if(
             q1.options === "Multiplication")
             {
                 let ans: number= q1.first * q1.second;
-                console.log(chalk.yellowBright("\n" + ans.toFixed(2)));
+                console.log("\nResult: " + chalk.yellowBright(ans.toFixed(2)));
             }
             else if(
                 q1.options === "Division")
                 {
                     let ans: number= q1.first / q1.second;
-                    console.log(chalk.yellowBright("\n" + ans.toFixed(2)));
+                    console.log("\nResult: " + chalk.yellowBright(ans.toFixed(2)));
                 }
                 else if(
                     q1.options === "Modulo")
                     {
                         let ans: number= q1.first % q1.second;
-                        console.log(chalk.yellowBright("\n" + ans.toFixed(2)));
+                        console.log("\nResult: " + chalk.yellowBright(ans.toFixed(2)));
                     }
 
 
